ff91039e973cd0c712b023742b3ded7195b811c0
Icezealots <161703198+Icezealots@users.noreply.github.com>
2025-05-03 23:23:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { Pool } = require('pg');
 const app = express();
-const port = process.env.PORT || 3000;
+
 
 // Secret 驗證
 const SECRET = process.env.WIX_SECRET;
